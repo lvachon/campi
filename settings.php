5def56180ba2f605337fbe7ffeb2f7868f2bacee
lvachon <lvachon@gmail.com>
2020-07-09 22:04:14 -0400

    no message

--- settings.php
+++ settings.php
@@ -2,6 +2,6 @@
 $vars = explode(",","shutter,iso,awbred,awbblue");
 $output = "";
 foreach($vars as $var){
-	$output.="{$var}=".strval(floatval($_GET[$var])).";";
+	$output.="{$var}:".strval(floatval($_GET[$var])).";";
 }
 file_put_contents("./ramdisk/settings",$output);
