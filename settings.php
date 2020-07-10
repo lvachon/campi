a01892fd3b237a857348e06658c380a7728fb247
lvachon <lvachon@gmail.com>
2020-07-09 21:46:06 -0400

    right...php (split vs explode)

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php 
-$vars = "again,dgain,shutter,iso,awbred,awbblue".split(",");
+$vars = explode(",","again,dgain,shutter,iso,awbred,awbblue");
 $output = "";
 foreach($vars as $var){
 	$output.="{$var}=".strval(floatval($_GET[$var])).";";
