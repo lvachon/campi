c60143842cb83730dadd0905fa8d0f4760b09aaf
lvachon <lvachon@gmail.com>
2020-07-09 21:52:45 -0400

    Removed cruft

--- manual_cam.py
+++ manual_cam.py
@@ -2,8 +2,6 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import os
-import numpy as np
-import cv2
 import time
 
 width=1024
@@ -45,30 +43,4 @@ while(1):
 	f.close()
 	camera.capture('./ramdisk/frame.jpg')
 	time.sleep(1000)
-
-#
-#	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-#	print("Frame")
-#	cleanImage = frame.array.copy()
-#	
-#		f = open("./html/ramdisk/autocmd","r")
-#		if(f.mode=='r' and f.read()=="GO"):
-#			if lMaxY<maxY and cMaxY<maxY and rMaxY<maxY:
-#				os.system('cd html;./fwd.sh')
-#				print("    No walls")
-#			elif lMaxY<maxY and (rMaxY>maxY or cMaxY>maxY):
-#				os.system('cd html;./left.sh')
-#				print("    Going left")
-#			elif rMaxY<maxY and (lMaxY>maxY or cMaxY>maxY):
-#				os.system('cd html;./right.sh')
-#				print("    Going right")
-#			elif cMaxY>maxY or (lMaxY>maxY and rMaxY>maxY):
-#				print("    Backing up")
-#				os.system('cd html;./bwd.sh;./left.sh;./left.sh;./left.sh')
-#		f.close()
-#		#time.sleep(0.1)
-#		print("    Writing images")
-#		cv2.imwrite('./ramdisk/frame.jpg',cleanImage, [int(cv2.IMWRITE_JPEG_QUALITY), 33])
-#	rawCapture.truncate(0)
-#	laserOn = 1-laserOn
 
